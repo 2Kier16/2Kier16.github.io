44e47985bd7421b4f7317522874c46dd656082c1
Kier Turpin <99277532+2Kier16@users.noreply.github.com>
2026-04-17 09:40:21 -0400

    updates

--- update_nav.py
+++ update_nav.py
@@ -13,14 +13,10 @@ for file in files:
     def repl(match):
         nav_block = match.group(0)
         
-        # Determine the base links by extracting them or we can just reconstruct based on what's there
-        # Let's see what's currently in the nav
         has_home_hash = 'href="index.html#home"' in nav_block
-        has_resume_prof = 'href="resume.html">Professional Profile' in nav_block
         has_contact = 'href="index.html#contact"' in nav_block
         has_download = 'href="moodmix.html#download"' in nav_block
         
-        # Build the new block
         new_nav = '            <nav class="nav-links">\n'
         
         if has_home_hash:
@@ -29,25 +25,22 @@ for file in files:
             new_nav += '                <a href="index.html">Home</a>\n'
             
         new_nav += '                <a href="moodmix.html">MoodMix</a>\n'
-        new_nav += '                <a href="heritage.html">The Heritage Project</a>\n'
+        new_nav += '                <a href="heritage.html">Heritage Project</a>\n'
         
         arrow = '▾' if '▾' in nav_block else '▼'
         if '▼' not in nav_block and '▾' not in nav_block:
              arrow = '▼'
              
         new_nav += '                <div class="dropdown">\n'
-        new_nav += f'                    <a href="#" onclick="event.preventDefault()">KVT Links {arrow}</a>\n'
+        new_nav += f'                    <a href="#" onclick="event.preventDefault()">Pathways {arrow}</a>\n'
         new_nav += '                    <div class="dropdown-content">\n'
         new_nav += '                        <a href="community.html">Community</a>\n'
         new_nav += '                        <a href="music.html">Music</a>\n'
-        new_nav += '                        <a href="veteran-resources.html">Veteran Resources</a>\n'
+        new_nav += '                        <a href="veteran-resources.html">Vet Resources</a>\n'
         new_nav += '                    </div>\n'
         new_nav += '                </div>\n'
         
-        if has_resume_prof:
-            new_nav += '                <a href="resume.html">Professional Profile</a>\n'
-        else:
-            new_nav += '                <a href="resume.html">Resume</a>\n'
+        new_nav += '                <a href="resume.html">Resume</a>\n'
             
         new_nav += '                <a href="about.html">About</a>\n'
         
